51a23ad3610f143d7364c44c4c577b6697a1e91f
Alex Darby <alexdarby64@gmail.com>
2016-06-28 23:44:31 +0100

    Started updating MyEtherContract to include security functions.

--- EtherBank/EtherBank/DecentralizedApp/sol/MyEtherBank.sol
+++ EtherBank/EtherBank/DecentralizedApp/sol/MyEtherBank.sol
@@ -1,35 +1,58 @@
 contract MyEtherBank 
 {
     /* -------- State data -------- */
+
     address _owner;
-    uint256 _bankDonationsBalance;
+    bool private _openNewBankAccountsEnabled;
+    uint256 private _bankDonationsBalance;
    
-    // Account               
+    // Account      
+    struct BankAccountAddress
+    {
+        uint32 accountNumber;
+        address accountOwner;        
+    }   
+
     struct BankAccount 
     {
         bool accountSet;
-        uint256 accountNumber;
-        address accountOwner;
+        uint32 accountNumber;
+            address accountOwner;
         uint256 balance;
+        bytes32 passwordSha3Hash;
     }
 
     // Account owners
-    mapping(address => BankAccount) public _bankAccounts;
+    mapping(address => BankAccount) private _bankAccounts;
+    BankAccountAddress[] private _bankAccountsAddressArray; 
 
     // Total bank accounts
-    uint256 _totalBankAccounts;
+    uint32 private _totalBankAccounts;
 
 
     /* -------- Constructor -------- */
+
     function MyEtherBank()
     {
         // Set the contract owner
-        _owner = msg.sender;  
+        _owner = msg.sender; 
+        _openNewBankAccountsEnabled = true; 
         _bankDonationsBalance = 0; 
     }
 
 
     /* -------- Modifiers -------- */
+
+    modifier modifier_isContractOwner()
+    { 
+        // Value sent?
+        if (msg.sender != _owner)
+        {
+            throw;       
+        }
+        _ 
+    }
+
     modifier modifier_doesSenderHaveABankAccount() 
     { 
         // Does this sender have a bank account?
@@ -51,16 +74,20 @@ contract MyEtherBank
         _ 
     }
 
+
     /* -------- Events -------- */
+
     event event_bankAccountOpened(address indexed bankAccountOwner, uint256 indexed bankAccountNumber);
     event event_depositMadeToBankAccount(uint256 indexed bankAccountNumber, uint256 indexed depositAmount); 
     event event_withdrawalMadeFromBankAccount(uint256 indexed bankAccountNumber, uint256 indexed withdrawalAmount); 
     event event_transferMadeFromBankAccountToAddress(uint256 indexed bankAccountNumber, uint256 indexed withdrawalAmount, address indexed destinationAddress); 
+	event event_bankDonationsWithdrawn(uint256 donationsAmount);
+	event event_securityPasswordHashAddedToBankAccount(uint256 indexed bankAccountNumber);
 
 
     /* -------- Contract owner functions -------- */
 
-    function DonateToBank(uint256 amount)
+    function Donate(uint256 amount)
     {
         if (amount > 0)
         {
@@ -68,23 +95,77 @@ contract MyEtherBank
         }
     }
 
+    function BankOwner_WithdrawDonations(address destinationAddress)
+        modifier_isContractOwner()
+    { 
+        if (_bankDonationsBalance > 0)
+        {
+            uint256 amount_ = _bankDonationsBalance;
+            _bankDonationsBalance = 0;
+
+            // Check if using send() is successful
+            if (!msg.sender.send(amount_))
+            {
+                throw;
+            }
+            else
+            {
+                event_bankDonationsWithdrawn(amount_);
+            }
+        }
+    }
+
+    function BankOwner_EnableNewBankAccountsToBeAdded()
+        modifier_isContractOwner()
+    { 
+        if (_openNewBankAccountsEnabled == false)
+        {
+            _openNewBankAccountsEnabled = true;
+        }
+    }
+
+    function BankOwner_DisableNewBankAccountsToBeAdded()
+        modifier_isContractOwner()
+    { 
+        if (_openNewBankAccountsEnabled)
+        {
+            _openNewBankAccountsEnabled = false;
+        }
+    }
+
+
+
+
 
 
     /* -------- General bank functions -------- */
 
     // Open bank account
     function OpenBankAccount()
-        returns (uint256 accountNumber)
+        returns (uint32 accountNumber)
     {
+        // Can new bankaccounts be openned?
+        if ( _openNewBankAccountsEnabled == false)
+        {
+            throw;        
+        }
+
         // Does this sender already have a bank account?
         if (_bankAccounts[msg.sender].accountSet)
         {
-            // Throw
             throw;
         }
 
         // Assign the new bank account number
         accountNumber = _totalBankAccounts;
+
+        // Add new account to the array
+        _bankAccountsAddressArray.push( BankAccountAddress(
+            {
+                accountNumber: accountNumber,
+                accountOwner: msg.sender
+            }
+            ));
 
         // Add the new account
         _bankAccounts[msg.sender].accountNumber = accountNumber;
@@ -256,6 +337,24 @@ contract MyEtherBank
         return false;
     }
 
+
+    /* -------- security functions -------- */
+
+    function Security_AddPasswordSha3HashToAccount(bytes32 hash)
+        modifier_doesSenderHaveABankAccount()
+        modifier_wasValueSent()
+        returns (bool)
+    {
+        
+
+
+
+        // Set the account password hash
+        _bankAccounts[msg.sender].passwordSha3Hash = hash;
+
+        event_securityPasswordHashAddedToBankAccount(_bankAccounts[msg.sender].accountNumber);
+        return true;
+    }
 
 
 
